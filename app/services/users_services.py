684d2cc4f4544d89e06ed9d7d037d260f9f2a571
lautajam <lautajamdev@gmail.com>
2025-04-19 19:24:40 -0300

    format

--- app/services/users_services.py
+++ app/services/users_services.py
@@ -2,77 +2,82 @@ from sqlalchemy.orm import Session
 from models.model import User
 from schemas.schema import UserCreate, UserUpdate
 
-def get_all_users(db : Session):
-    """ Get all users from the database.
-    
+
+def get_all_users(db: Session):
+    """Get all users from the database.
+
     Args:
         db (Session): SQLAlchemy session object
 
     Returns:
         List[User]: a list of User objects
     """
-    
+
     return db.query(User).all()
 
+
 def get_user_by_id(db: Session, user_id: int):
-    """ Get a user by ID from the database.
-   
+    """Get a user by ID from the database.
+
     Args:
         db (Session): SQLAlchemy session object
         user_id (int): ID of the user to retrieve
-        
+
     Returns:
         User: User object if found, None otherwise
     """
-    
+
     return db.query(User).filter(User.user_id == user_id).first()
 
+
 def create_user(db: Session, user_data: UserCreate):
-    """ Create a new user in the database.
-    
+    """Create a new user in the database.
+
     Args:
         db (Session): SQLAlchemy session object
         user_data (UserCreate): User data to create a new user
-        
+
     Returns:
         User: Created User object
     """
-    
+
     new_user = User(**user_data.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
 
+
 def delete_user_by_id(db: Session, user_id: int):
-    """ Delete a user by ID from the database.
-    
+    """Delete a user by ID from the database.
+
     Args:
         db (Session): SQLAlchemy session object
         user_id (int): ID of the user to delete
-        
+
     Returns:
         User: Deleted User object if found, None otherwise
     """
-    
+
     delete_user = get_user_by_id(db, user_id)
     if delete_user:
         db.delete(delete_user)
         db.commit()
     return delete_user
 
+
 def update_user(db: Session, user_id: int, user_update: UserUpdate):
-    """ Update a user in the database.
-    
+    """Update a user in the database.
+
     Args:
         db (Session): SQLAlchemy session object
         user_id (int): ID of the user to update
         user_update (UserUpdate): User data to update
-        
+
     Returns:
         User: Updated User object if found, None otherwise
     """
-    
+
     user = db.query(User).filter(User.user_id == user_id).first()
     if not user:
         return False
