79952ba851de1eebd6417ffa20fe835655c758b7
lautajam <lautajamdev@gmail.com>
2025-04-22 10:30:18 -0300

    verify users upgrade

--- ui/tools/login.py
+++ ui/tools/login.py
@@ -3,22 +3,23 @@ from api.endpoints import USER_ENDPOINTS
 import requests
 import os
 
-def verify_user(password: str, username: str):
-    try:
-        list_response_users_password = requests.get(USER_ENDPOINTS['get_user_by_password'](password))
-        
-        if not list_response_users_password:
-            return 0
-        
-        list_response_users_password_json = list_response_users_password.json()
 
-        for user in list_response_users_password_json:
-            if user['username'] == username:
-                return 1
-        
+def verify_user(username: str, password: str):
+    try:
+        response_user_password = requests.get(
+            USER_ENDPOINTS["get_user_by_password_username"](username, password)
+        )
+
+        if not response_user_password:
+            return 0
+
+        response_user_password_json = response_user_password.json()
+
+        if response_user_password_json["username"] == username:
+            return 1
+
         return 0
     except Exception as e:
-        
+
         print(f"Error: {e}")
         return -1
-    
