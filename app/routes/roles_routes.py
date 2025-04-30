3f633b585cd143ce6902ded2cb1e319cd997ca8d
lautajam <lautajamdev@gmail.com>
2025-04-30 00:19:31 -0300

    delete roles already

--- app/routes/roles_routes.py
+++ app/routes/roles_routes.py
@@ -3,15 +3,11 @@ import json
 import urllib.parse
 from typing import List
 from database import get_db
-from dotenv import load_dotenv
 from sqlalchemy.orm import Session
 from fastapi.responses import JSONResponse
-from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form
+from fastapi import APIRouter, Depends, HTTPException, Form
 from schemas.role_schema import RoleCreate, RoleUpdate, RoleResponse
 from services import roles_service
-
-load_dotenv()
-s3_bucket_name = os.getenv("S3_BUCKET")
 
 router = APIRouter()
 
@@ -28,6 +24,7 @@ delete_role_by_id
 
 update_role
 """
+
 
 @router.post("/get-roles", response_model=List[RoleResponse])
 def get_all_roles(db: Session = Depends(get_db)):
@@ -50,17 +47,18 @@ def get_all_roles(db: Session = Depends(get_db)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error obtaining roles. {str(e)}")
 
+
 @router.post("/get-role-id", response_model=RoleResponse)
 def get_role_by_id(role_id: int, db: Session = Depends(get_db)):
     """Get a role by ID from the database.
-    
+
     Args:
         role_id (int): ID of the role to retrieve
         db (Session): SQLAlchemy session object
-        
+
     Returns:
         Role: Role object if found, None otherwise
-        
+
     Raises:
         HTTPException: If the role is not found or if there is an error during the retrieval.
     """
@@ -71,18 +69,19 @@ def get_role_by_id(role_id: int, db: Session = Depends(get_db)):
         return role
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error obtaining roles. {str(e)}")
-    
+
+
 @router.post("/get-role-name", response_model=RoleResponse)
 def get_role_by_name(role_name: str, db: Session = Depends(get_db)):
     """Get a role by name from the database.
-    
+
     Args:
         role_name (str): Name of the role to retrieve
         db (Session): SQLAlchemy session object
-        
+
     Returns:
         Role: Role object if found, None otherwise
-        
+
     Raises:
         HTTPException: If the role is not found or if there is an error during the retrieval.
     """
@@ -93,3 +92,27 @@ def get_role_by_name(role_name: str, db: Session = Depends(get_db)):
         return role
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error obtaining roles. {str(e)}")
+
+
+@router.delete("/delete-role/{role_id}")
+def delete_user_by_id(role_id: int, db: Session = Depends(get_db)):
+    """Delete a role by ID from the database.
+
+    Args:
+        role_id (int): ID of the role to delete
+        db (Session): SQLAlchemy session object
+
+    Returns:
+        JSONResponse: JSON response with a success message
+
+    Raises:
+        HTTPException: If the role is not found or if there is an error during the deletion.
+    """
+    try:
+        roles_service.delete_user_by_id(db, role_id)
+        return JSONResponse(
+            status_code=200,
+            content={"message": f"Roel #{role_id} deleted successfully"},
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error deleting role. {str(e)}")
