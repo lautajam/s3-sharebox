1dbb9fea2aa8b00843cd1dd31ff6907573e6dbe6
lautajam <lautajamdev@gmail.com>
2025-04-30 03:13:52 -0300

    now files can only be deleted by admins or the owner

--- app/routes/file_routes.py
+++ app/routes/file_routes.py
@@ -9,6 +9,7 @@ from fastapi.responses import JSONResponse
 from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form
 
 from services import files_services
+#from models.model import File as FileModel
 from schemas.file_schema import FileCreate, FileUpdate, FileResponse
 from utils.files_managment import (
     save_file_temporarily,
@@ -21,6 +22,33 @@ s3_bucket_name = os.getenv("S3_BUCKET")
 router = APIRouter()
 
 
+@router.get("/get-file-name/{file_name}", response_model=FileResponse)
+def get_file_by_name_in_db(file_name: str, db: Session = Depends(get_db)):
+    """This function is used to get a file by its name from the database.
+
+    Args:
+        file_name (str): The name of the file to be retrieved.
+        db (Session): SQLAlchemy session object.
+
+    Returns:
+        FileResponse: The file data if found.
+
+    Raises:
+        HTTPException: If the file is not found in the database.
+        HTTPException: If there is an error during the retrieval.
+    """
+    try:
+        file_data = files_services.get_file_by_name_in_db(db, file_name)
+
+        if not file_data:
+            raise HTTPException(status_code=404, detail="File not found")
+
+        return file_data
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error retrieving file: {str(e)}")
+
+
 @router.post("/upload-register-file", response_model=FileResponse)
 async def upload_and_register_file(
     uploaded_file: UploadFile = File(...),
@@ -28,7 +56,7 @@ async def upload_and_register_file(
     owner_id: int = Form(1),
     db: Session = Depends(get_db),
 ):
-    """ This function first registers the file in the database and then uploads the file to S3.
+    """This function first registers the file in the database and then uploads the file to S3.
 
     Args:
         uploaded_file (UploadFile): The uploaded file.
@@ -70,7 +98,7 @@ async def register_file_in_db(
     owner_id: int = Form(1),
     db: Session = Depends(get_db),
 ):
-    """ This function is used to register a file in the database.
+    """This function is used to register a file in the database.
 
     Args:
         uploaded_file (UploadFile): The uploaded file.
@@ -105,7 +133,7 @@ async def register_file_in_db(
 async def upload_file_to_s3(
     uploaded_file: UploadFile = File(...),
 ):
-    """ This function is used to upload a file to S3.
+    """This function is used to upload a file to S3.
 
     Args:
         uploaded_file (UploadFile): The uploaded file.
@@ -133,46 +161,83 @@ async def upload_file_to_s3(
             content={"message": "File uploaded to S3 successfully"},
         )
 
-    except Exception as e:
+    except Exception:
         raise HTTPException(status_code=500, detail=f"Failed to upload file: {str(e)}")
 
 
-@router.delete("/delete-file/{file_id}/{file_name}")
-def delete_file(file_id: int, file_name: str, db: Session = Depends(get_db)):
-    """ This function is used to delete a file from S3 and the database.
+@router.delete("/delete-file/{file_id}/{file_name}/{user_id}")
+def delete_file(
+    file_id: int, file_name: str, user_id: int, db: Session = Depends(get_db)
+):
+    """
+    Delete a file from the database and S3. If deletion from S3 fails, restore DB record.
 
     Args:
-        file_id (int): The ID of the file to be deleted.
-        file_name (str): The name of the file to be deleted.
+        file_id (int): ID of the file to delete.
+        file_name (str): Name of the file.
+        user_id (int): ID of the user requesting deletion.
+        db (Session): DB session.
 
     Returns:
-        JSONResponse: A response indicating success or failure.
-
-    Raises:
-        HTTPException: If there is an error during the deletion.
-        HTTPException: If the file is not found in the database.
+        JSONResponse
     """
     try:
+        file = files_services.get_file_by_name_in_db(db, file_name)
+        if file is None:
+            raise HTTPException(status_code=404, detail="File not found in database")
 
-        # Hay que hacer un chequeo de que el file_name sea del file_id
+        if file.file_id != file_id:
+            raise HTTPException(status_code=400, detail="File ID and name do not match")
 
-        # Eliminar de S3
-        delete_file_fom_s3(file_name)
+        file_backup = {
+            "file_name": file.file_name,
+            "file_metadata": file.file_metadata,
+            "file_type": file.file_type,
+            "folder_id": file.folder_id,
+            "owner_id": file.owner_id,
+            "s3_url": file.s3_url,
+            "uploaded_at": file.uploaded_at,
+        }
+        file_name_extension = f"{file_backup['file_name']}{file_backup['file_type']}"
+        
+        result, message = files_services.delete_file_from_db(db, file_id, user_id)
+        if result <= 0:
+            raise HTTPException(status_code=403 if result == 0 else 500, detail=message)
 
-        # Eliminar de la base de datos
-        delete_file_fom_db(file_id, db)
+        s3_result = files_services.delete_file_from_s3(
+            db, s3_bucket_name, file_name_extension.strip()
+        )
+        if s3_result is None:
+            try:
+                restored_file = File(**file_backup)
+                db.add(restored_file)
+                db.commit()
 
+            except Exception as e:
+                db.rollback()
+                raise HTTPException(
+                    status_code=500,
+                    detail=f"Error restoring file in DB after S3 deletion failure: {str(e)}",
+                )
+
+            raise HTTPException(
+                status_code=500,
+                detail="Error deleting file from S3. File restored in DB.",
+            )
+            
         return JSONResponse(
             status_code=200,
-            content={"message": "File deleted successfully from S3 and database"},
+            content={"message": "File deleted successfully"},
         )
 
+    except HTTPException as e:
+        raise e
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error deleting file: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
 
 
-@router.delete("/delete-file-from-db/{file_id}")
-def delete_file_fom_db(file_id: int, db: Session = Depends(get_db)):
+@router.delete("/delete-file-from-db/{file_id}/{user_id}")
+def delete_file_fom_db(file_id: int, user_id, db: Session = Depends(get_db)):
     """This function is used to delete a file from the database.
 
     Args:
@@ -188,39 +253,36 @@ def delete_file_fom_db(file_id: int, db: Session = Depends(get_db)):
     """
 
     try:
-        flag = files_services.delete_file_from_db(db, file_id)
+        flag = files_services.delete_file_from_db(db, file_id, user_id)
 
         if flag[0] == 0:
             raise HTTPException(status_code=404, detail=f"{flag[1]}")
         elif flag[0] == -1:
             raise HTTPException(status_code=500, detail=f"{flag[1]}")
 
-        return JSONResponse(
-            status_code=200,
-            content={"message": f"flag[1]"},
-        )
-
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error deleting file. {str(e)}")
 
 
 @router.delete("/delete-file-from-s3/{file_name}")
-def delete_file_fom_s3(file_name: str):
-    """ This function is used to delete a file from S3.
-    
+def delete_file_fom_s3(file_name: str, db: Session = Depends(get_db)):
+    """This function is used to delete a file from S3.
+
     Args:
         file_name (str): The name of the file to be deleted.
-        
+
     Returns:
         JSONResponse: A response indicating success or failure.
-        
+
     Raises:
         HTTPException: If there is an error during the deletion.
         HTTPException: If the file is not found in S3.
     """
 
     try:
-        response = files_services.delete_file_from_s3(s3_bucket_name, file_name.strip())
+        response = files_services.delete_file_from_s3(
+            db, s3_bucket_name, file_name.strip()
+        )
 
         if response is None:
             return JSONResponse(
--- app/services/files_services.py
+++ app/services/files_services.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from sqlalchemy.orm import Session
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 
-from models.model import File
+from models.model import File, User
 from schemas.file_schema import FileCreate, FileUpdate
 
 load_dotenv()
@@ -13,6 +13,38 @@ aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
 aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 aws_region = os.getenv("AWS_REGION")
 
+ADMIN_ROLE_ID = 1
+
+def get_file_by_name_in_s3(db: Session, file_name: str, aws_bucket_name: str) -> File:
+    """Get a file from S3 bucket by its name.
+    
+    Args:
+        db (Session): SQLAlchemy session object
+        file_name (str): Name of the file to retrieve
+        aws_bucket_name (str): S3 bucket name
+        
+    Returns:
+        File: File object if found, None otherwise
+    """
+    s3 = boto3.client("s3")
+
+    try:
+        response = s3.list_objects_v2(Bucket=aws_bucket_name, Prefix=file_name)
+        return response
+    except Exception as e:
+        return None
+
+def get_file_by_name_in_db(db: Session, file_name: str) -> File:
+    """Get a file from the database by its name.
+
+    Args:
+        db (Session): SQLAlchemy session object
+        file_name (str): Name of the file to retrieve
+
+    Returns:
+        File: File object if found, None otherwise
+    """
+    return db.query(File).filter(File.file_name == file_name).first()
 
 def upload_file_to_s3(file_path, bucket_name, s3_file_name) -> bool:
     """
@@ -59,7 +91,7 @@ def create_file_to_db(db: Session, new_file_data: FileCreate):
     return new_file
 
 
-def delete_file_from_db(db: Session, file_id: int) -> Tuple[int, str]:
+def delete_file_from_db(db: Session, file_id: int, user_id: int) -> Tuple[int, str]:
     """Delete a file from the database.
 
     Args:
@@ -69,7 +101,13 @@ def delete_file_from_db(db: Session, file_id: int) -> Tuple[int, str]:
     Returns:
         bool: True if deletion was successful, False otherwise
     """
+
     try:
+        is_user_owner = db.query(File).filter(File.file_id == file_id, File.owner_id == user_id).first()
+        is_admin = db.query(User).filter(User.user_id == user_id, User.role_id == ADMIN_ROLE_ID).first()
+        if not (is_user_owner or is_admin):
+            return (0, "You are not the owner of this file or an admin")
+        
         file_to_delete = db.query(File).filter(File.file_id == file_id).first()
         if not file_to_delete:
             return (0, "File not found")
@@ -81,11 +119,23 @@ def delete_file_from_db(db: Session, file_id: int) -> Tuple[int, str]:
         return (-1, f"Error deleting file. {str(e)}")
 
 
-def delete_file_from_s3(s3_bucket_name: str, file_name: str) -> dict|str:
+def delete_file_from_s3(db: Session, s3_bucket_name: str, file_name: str) -> str:
+    """Delete a file from S3 bucket.
+    
+    Args:
+        db (Session): SQLAlchemy session object
+        s3_bucket_name (str): S3 bucket name
+        file_name (str): Name of the file to be deleted
+        
+    Returns:
+        str: Response from S3 delete operation or None if file not found
     """
     
-    """
-
+    file = get_file_by_name_in_db(db, file_name)
+    if file is not None:
+        file_type = file.file_type if file else None
+        
+        file_name = f"{file_name}{file_type}"
     s3 = boto3.client("s3")
 
     try:
