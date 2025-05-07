d16f1a7a9675ccf16bb34c3328b8257fe3b4fbce
lautajam <lautajamdev@gmail.com>
2025-05-07 17:40:29 -0300

    now the ui is already to filter

--- ui/config/set_sidebar.py
+++ ui/config/set_sidebar.py
@@ -6,7 +6,8 @@ def show_sidebar():
     """ This function creates the sidebar for the application. """
     with st.sidebar:
         st.markdown("## Menú")
-        st.button("Inicio")
+        st.button("Gestión de Archivos")
+        st.button("Subir archivo")
         st.button("Configuración")
         if st.button("Cerrar sesión", on_click=close_session, key="close_session"):
             st.rerun()
--- ui/tools/files_managment.py
+++
@@ -1,3 +0,0 @@
-from api.endpoints import FILES_ENDPOINTS
-from tools.constants import DEFAULT_ROLE
-import requests
